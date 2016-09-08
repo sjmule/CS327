78f86b2f7f407d734aa77f301220ca8b20807dd4
Scott Mueller <sjmule@comcast.net>
2016-09-08 15:21:53 -0500

    moved headers to one file

--- a1.02/dungeon.c
+++ a1.02/dungeon.c
@@ -1,9 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <time.h>
-#include <string.h>
 #include "generate.h"
 
 dungeon aincrad;
---
+++ a1.02/generate.h
@@ -0,0 +1,55 @@
+#ifndef GENERATE_H
+#define GENERATE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+
+#define X 81
+#define Y 21
+
+typedef struct ROOM room;
+
+struct ROOM
+{
+	int number;
+	int x;
+	int y;
+	int height;
+	int width;
+};
+
+typedef struct DUNGEON dungeon;
+
+struct DUNGEON
+{
+	char map[Y][X];
+	int hardness[Y][X];
+	int numRooms;
+	room rooms[8];
+};
+
+extern dungeon aincrad;
+
+void setImmutable();
+
+void setHardness();
+
+void setBoundary();
+
+void initializeDungeon();
+
+room generateRoom();
+
+int verifyValidity(room room, int x, int y);
+
+void placeRoom(room room, int x, int y);
+
+void createRooms();
+
+void connectRooms();
+
+#endif
