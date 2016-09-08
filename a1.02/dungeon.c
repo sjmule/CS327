daa3cf231cc5f086a92b63bc66f4ca42b0252cb4
Scott Mueller <sjmule@comcast.net>
2016-09-08 15:05:48 -0500

    create save folder, function stubs for load/save

---
+++ a1.02/dungeon.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <string.h>
+#include "generate.h"
+
+dungeon aincrad;
+
+void saveDungeon()
+{
+
+}
+
+void loadDungeon()
+{
+
+}
+
+void printDungeon(int debug)
+{
+	int i = 0;
+	for(; i < Y; ++i)
+	{
+		printf("%s\n", aincrad.map[i]);
+	}
+	if(debug)
+	{
+		for(i = 0; i < Y; ++i)
+		{
+			int j = 0;
+			for(; j < X; ++j)
+			{
+				printf("%d", aincrad.hardness[i][j]);
+			}
+			printf("\n");
+		}
+	}
+}
+
+int main(int argc, char** argv)
+{
+	srand((unsigned) time(0));
+	initializeDungeon();
+	createRooms();
+	connectRooms();
+	printDungeon(0);
+
+	char* home = getenv("HOME");
+	char* path = strcat(home, "/.rlg327");
+	printf("%s\n", path);
+	mkdir(path, 0777);
+
+	return 0;
+}
