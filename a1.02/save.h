37077e47be3d1ee13fc48b4246481ede3494e747
Scott Mueller <sjmule@comcast.net>
2016-09-12 20:16:14 -0500

    save/load functions moved to new files, Makefile updated

--- a1.02/save.h
+++ a1.02/save.h
@@ -6,6 +6,10 @@
 #define _itos(x,y) ((x >> y) & 0xFF)
 #define _stoi(x) (x[3] << 24) | (x[2] << 16) | (x[1] << 8) | (x[0])
 
+unsigned int version;
+
+FILE* file;
+
 void saveDungeon(char* path);
 
 void loadDungeon(char* path);
