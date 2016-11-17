e27ec6be43b9019ed031a0e6b31da122e7f828ea
Scott Mueller <sjmule@comcast.net>
2016-11-17 00:48:27 -0600

    assignment finished

--- a1.09/movement.h
+++ a1.09/movement.h
@@ -3,21 +3,13 @@
 
 #include "dungeon.h"
 
-int switchBoard(Monster* mon, int playerX, int playerY);
-
-int inRoom(int x, int y);
-
-void makeTunnel(int x, int y);
-
-int valid(int x, int y, int tunnel);
+Character* checkForOthers(Character* entity, int dir);
 
 int isMoveValid(Character* entity, int dir, int tunnel);
 
 void doMove(Character* entity, int dir);
 
-void moveRandom(Monster* character, int tunnel);
-
-void moveDeliberately(Monster* mon);
+void combat(Character* attack, Character* defense);
 
 void moveMonster(Monster* mon);
 
